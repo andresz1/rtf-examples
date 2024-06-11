48a1a0174349162c47a479ed4fefc5e67bea7629
Andrés Alvarez <a.alvarez.sor@gmail.com>
2024-06-11 19:50:33 +0200

    client components fix

--- src/app/examples/flat/page.tsx
+++ src/app/examples/flat/page.tsx
@@ -1,14 +1,18 @@
 "use client";
 
 import { FlatExample } from "@/components/FlatExample";
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
 
 export default function FlatExamplePage() {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
   return (
     <main className="w-screen h-screen">
-      <Suspense fallback={null}>
-        <FlatExample />
-      </Suspense>
+      <Suspense fallback={null}>{isMounted && <FlatExample />}</Suspense>
     </main>
   );
 }
